4d95de4009e1ff9992b4bd3a7976e0e9f0616f54
YooJunHyuk <a01091040305@gmail.com>
2025-12-08 19:26:55 +0900

    Update source code for run server

--- 01_backend/chicken_site_project/chicken_site_project/settings.py
+++ 01_backend/chicken_site_project/chicken_site_project/settings.py
@@ -4,20 +4,29 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+FRONTEND_DIR = Path(__file__).resolve().parent.parent.parent.parent
 
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [FRONTEND_DIR / '00_frontend' / '00_HTML'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+
+                # Use load static
+                'django.template.context_processors.static'
             ],
         },
     },
+]
+
+STATICFILES_DIRS = [
+    FRONTEND_DIR / '00_frontend' / '01_CSS',
+    FRONTEND_DIR / '00_frontend' / '02_JS'
 ]
 
 # Application definition
--- 01_backend/chicken_site_project/chicken_site_project/urls.py
+++ 01_backend/chicken_site_project/chicken_site_project/urls.py
@@ -2,19 +2,24 @@
 
 from django.contrib import admin
 from django.urls import path
+from django.views.generic import TemplateView
 from chicken_site_app import views
 
-# URL patterns
 urlpatterns = [
     # Admin
     path('admin/', admin.site.urls),
 
-    # User
+    # Frontend HTML
+    path('', TemplateView.as_view(template_name='index.html'), name='home'),
+    path('login.html', TemplateView.as_view(template_name='login.html'), name='login_page'),
+    path('sign_up.html', TemplateView.as_view(template_name='sign_up.html'), name='signup_page'),
+    path('upload.html', TemplateView.as_view(template_name='upload.html'), name='upload_page'),
+
+    # API
     path('signup/', views.signup, name='signup'),
     path('login/', views.login, name='login'),
     path('logout/', views.logout, name='logout'),
 
-    # Chicken
     path('chicken/list/', views.chicken_list, name='chicken_list'),
     path('chicken/create/', views.chicken_create, name='chicken_create'),
     path('chicken/update/<int:chicken_id>/', views.chicken_update, name='chicken_update'),
